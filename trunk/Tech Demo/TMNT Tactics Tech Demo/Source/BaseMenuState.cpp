11597f743473bd5d2d241fd636afbfffb96b210b
AllThingsCandid <AllThingsCandid@7dc79cba-3e6d-11de-b8bc-ddcf2599578a>
2009-05-26 07:07:32 +0000

    Latest tech demo build (with animations)
    - RAY

---
+++ trunk/Tech Demo/TMNT Tactics Tech Demo/Source/BaseMenuState.cpp
@@ -0,0 +1,65 @@
+#include "BaseMenuState.h"
+#include "Assets.h"
+#include "Game.h"
+#include "CSGD_TextureManager.h"
+#include "CSGD_Direct3D.h"
+#include "CSGD_DirectInput.h"
+#include "BitmapFont.h"
+//#include "fmod.hpp"
+
+CBaseMenuState::CBaseMenuState()
+{
+
+}
+CBaseMenuState::~CBaseMenuState()
+{
+
+}
+void CBaseMenuState::Render()
+{
+	m_pTM->Draw(m_nBGImageID, m_nBGX, m_nBGY);
+}
+
+void CBaseMenuState::Update(float fElapsedTime)
+{
+
+}
+
+void CBaseMenuState::Enter()
+{
+	m_pAssets		= CAssets::GetInstance();
+	m_pDI			= CSGD_DirectInput::GetInstance();
+	m_pTM			= CSGD_TextureManager::GetInstance();
+	m_nScreenWidth	= CGame::GetInstance()->GetScreenWidth();
+	m_nScreenHeight = CGame::GetInstance()->GetScreenHeight();
+	m_pBitmapFont   = CBitmapFont::GetInstance();
+
+	m_nMenuItemSpacing = 45;
+	//m_pFMOD		= CGame::GetInstance()->GetFMODSystem();
+}
+
+bool CBaseMenuState::Input(float elapsedTime, POINT mousePt)
+{
+
+	return true;
+}
+
+void CBaseMenuState::Exit()
+{
+	if (m_pAssets)
+		m_pAssets = NULL;
+	if (m_pTM)
+		m_pTM = NULL;
+	if (m_pDI)
+		m_pDI = NULL;
+// 	if (m_pBitmapFont)
+// 		m_pBitmapFont = NULL;
+// 	if (m_pFMOD)
+// 		m_pFMOD = NULL;
+}
+
+void CBaseMenuState::CenterBGImage()
+{
+	m_nBGX = (m_nScreenWidth >> 1) - (m_nImageWidth >> 1);
+	m_nBGY = (m_nScreenHeight >> 1) - (m_nImageHeight >> 1);
+}

7db0c3e0ac7ec7d7313defc9aa42d4207021e7e4
AllThingsCandid <AllThingsCandid@7dc79cba-3e6d-11de-b8bc-ddcf2599578a>
2009-05-26 23:28:25 +0000

    With CParticleSystem, latest build - RAY 7:25 pm tuesday

---
+++ trunk/Tech Demo/TMNT Tactics Tech Demo/Source/Assets.cpp
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////
+//	Filename	:	Assets.cpp
+//
+//	Author		:	Ramon Johannessen (RJ)
+//
+//	Purpose		:	To define the CAssets class. Will load all assets used
+//					within the game and hold the IDs. A singleton.
+//////////////////////////////////////////////////////////////////////////
+#include "Assets.h"
+#include "CSGD_TextureManager.h"
+#include "CSGD_Direct3D.h"
+
+CAssets* CAssets::GetInstance()
+{
+	static CAssets instance;
+	return &instance;
+}
+
+CAssets::CAssets()
+{
+	m_pTM = CSGD_TextureManager::GetInstance();
+	LoadAssets();
+}
+CAssets::~CAssets()
+{
+
+}
+
+void CAssets::LoadAssets()
+{
+	//////////////////////////////////////////////////////////////////////////
+	// globally used IDs
+	aBitmapFontID		= m_pTM->LoadTexture("Resources/Images/VG_BitmapFont.png", D3DCOLOR_XRGB(0,0,0));
+	aMenuCursorImageID	= m_pTM->LoadTexture("Resources/Images/VG_menuCursor.bmp", D3DCOLOR_XRGB(255,255,255));
+	//////////////////////////////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////////////////////////////
+	// Battle Map IDs
+	aBMcursorID			= m_pTM->LoadTexture("Resources/Images/VG_Cursor.png", D3DCOLOR_XRGB(0,0,0));
+
+	//////////////////////////////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////////////////////////////
+	// Main Menu IDs
+	aMMBGimageID		= m_pTM->LoadTexture("Resources/Images/VG_MainMenuBG.png", D3DCOLOR_XRGB(255,255,255));
+
+	//////////////////////////////////////////////////////////////////////////
+}

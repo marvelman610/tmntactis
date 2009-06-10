04b5c5c0ab51e6b86e864e1edba434f244418af0
marvelman610 <marvelman610@7dc79cba-3e6d-11de-b8bc-ddcf2599578a>
2009-06-10 10:08:21 +0000

    alt+tab pauses alt+enter fixed

--- NewAlpha/TMNT Tactics Tech Demo/Source/Game.cpp
+++ NewAlpha/TMNT Tactics Tech Demo/Source/Game.cpp
@@ -147,12 +147,18 @@ bool CGame::Main(POINT mouse)
 	
 	// input, update, render
 	m_pDI->ReadDevices();
-	if ( ( m_pDI->KeyDown(DIK_RETURN) && m_pDI->KeyDown(DIK_LMENU) ) || ( m_pDI->KeyDown(DIK_RETURN) && m_pDI->KeyDown(DIK_RMENU) ) )
+	/*if ( ( m_pDI->KeyDown(DIK_RETURN) && m_pDI->KeyDown(DIK_LMENU) ) || ( m_pDI->KeyDown(DIK_RETURN) && m_pDI->KeyDown(DIK_RMENU) ) )
 	{
 		m_pD3D->ChangeDisplayParam(m_pD3D->GetPresentParams()->BackBufferWidth, m_pD3D->GetPresentParams()->BackBufferHeight, m_pD3D->GetPresentParams()->Windowed);
 		m_pDI->ReadDevices();
-	}
+	}*/
 	
+	if ((m_pDI->KeyDown(DIK_RMENU) && m_pDI->KeyDown(DIK_RETURN))  || (m_pDI->KeyDown(DIK_LMENU) && m_pDI->KeyDown(DIK_RETURN)) )
+	{
+		m_pD3D->ChangeDisplayParam(m_pD3D->GetPresentParams()->BackBufferWidth, m_pD3D->GetPresentParams()->BackBufferHeight, !m_pD3D->GetPresentParams()->Windowed);
+		m_pDI->ReadDevices();
+	}
+
 	if(m_pCurrentState->Input(m_fElapsedTime, mouse) == false)
 		return false;
 
